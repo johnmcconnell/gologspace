e6164f4b8c65c8c48eae5f8b7a0ecea23ea5c3c6
johnmcconnell <johnnyillinois@gmail.com>
2015-05-02 21:26:27 -0500

    adding more helpers

--- gologspace.go
+++ gologspace.go
@@ -4,6 +4,14 @@ import(
 	"math"
 )
 
+func LogAddAll(logX float64, logYs ...float64) float64 {
+	result := logX
+	for _, logY := range logYs {
+		result = LogAdd(result, logY)
+	}
+	return result
+}
+
 func LogAdd(logX, logY float64) float64 {
 	if logY > logX {
 		temp := logX
@@ -18,4 +26,32 @@ func LogAdd(logX, logY float64) float64 {
 		return logX
 	}
 	return logX + math.Log(1.0 + math.Exp(negDiff))
+}
+
+func LogDivAll(logX float64, logYs ...float64) float64 {
+	result := logX
+	for _, logY := range logYs {
+		result = LogDiv(result, logY)
+	}
+	return result
+}
+
+func LogDiv(logX, logY float64) float64 {
+	return logX - logY
+}
+
+func LogMultAll(logX float64, logYs ...float64) float64 {
+	result := logX
+	for _, logY := range logYs {
+		result = LogMult(result, logY)
+	}
+	return result
+}
+
+func LogMult(logX, logY float64) float64 {
+	return logX + logY
+}
+
+func LogProb(p float64) float64 {
+	return math.Log(p)
 }
